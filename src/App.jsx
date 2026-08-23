34459324e25106efed2e2aa7436388bcd3709976
Kasundi Ranaweera <kasundiranaweera@gmail.com>
2026-08-23 22:16:59 +0530

    Add all recruiter routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import { useAuth } from './auth/AuthContext'
 import { PrivateRoute, RecruiterRoute, CandidateRoute } from './routes/ProtectedRoute'
 
-import LoginPage            from './pages/LoginPage'
-import RegisterPage         from './pages/RegisterPage'
-import UnauthorizedPage     from './pages/UnauthorizedPage'
-import JobsPage             from './pages/candidate/JobsPage'
-import MyApplicationsPage   from './pages/candidate/MyApplicationsPage'
-import RecruiterDashboard   from './pages/recruiter/RecruiterDashboardPage'
-import RecruiterJobsPage    from './pages/recruiter/RecruiterJobsPage'
+import LoginPage              from './pages/LoginPage'
+import RegisterPage           from './pages/RegisterPage'
+import UnauthorizedPage       from './pages/UnauthorizedPage'
+import JobsPage               from './pages/candidate/JobsPage'
+import JobDetailPage          from './pages/candidate/JobDetailPage'
+import MyApplicationsPage     from './pages/candidate/MyApplicationsPage'
+import RecruiterDashboardPage from './pages/recruiter/RecruiterDashboardPage'
+import RecruiterJobsPage      from './pages/recruiter/RecruiterJobsPage'
+import CreateJobPage          from './pages/recruiter/CreateJobPage'
+import EditJobPage            from './pages/recruiter/EditJobPage'
+import JobApplicantsPage      from './pages/recruiter/JobApplicantsPage'
 
 function HomeRedirect() {
   const { user } = useAuth()
@@ -20,16 +24,23 @@ function HomeRedirect() {
 export default function App() {
   return (
     <Routes>
+      {/* Public */}
       <Route path="/login"        element={<LoginPage />} />
       <Route path="/register"     element={<RegisterPage />} />
       <Route path="/unauthorized" element={<UnauthorizedPage />} />
       <Route path="/"             element={<HomeRedirect />} />
 
+      {/* Candidate */}
       <Route path="/jobs"            element={<PrivateRoute><JobsPage /></PrivateRoute>} />
+      <Route path="/jobs/:id"        element={<PrivateRoute><JobDetailPage /></PrivateRoute>} />
       <Route path="/my-applications" element={<CandidateRoute><MyApplicationsPage /></CandidateRoute>} />
 
-      <Route path="/recruiter/dashboard" element={<RecruiterRoute><RecruiterDashboard /></RecruiterRoute>} />
-      <Route path="/recruiter/jobs"      element={<RecruiterRoute><RecruiterJobsPage /></RecruiterRoute>} />
+      {/* Recruiter */}
+      <Route path="/recruiter/dashboard"        element={<RecruiterRoute><RecruiterDashboardPage /></RecruiterRoute>} />
+      <Route path="/recruiter/jobs"             element={<RecruiterRoute><RecruiterJobsPage /></RecruiterRoute>} />
+      <Route path="/recruiter/jobs/create"      element={<RecruiterRoute><CreateJobPage /></RecruiterRoute>} />
+      <Route path="/recruiter/jobs/:id/edit"    element={<RecruiterRoute><EditJobPage /></RecruiterRoute>} />
+      <Route path="/recruiter/jobs/:id/applicants" element={<RecruiterRoute><JobApplicantsPage /></RecruiterRoute>} />
 
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
