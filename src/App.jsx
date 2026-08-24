626e707f25ace359449571910268211542f3c2d2
Kasundi Ranaweera <kasundiranaweera@gmail.com>
2026-08-24 13:59:20 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -20,28 +20,18 @@ function HomeRedirect() {
 export default function App() {
   return (
     <Routes>
-      {/* Public */}
       <Route path="/login"        element={<LoginPage />} />
       <Route path="/register"     element={<RegisterPage />} />
       <Route path="/unauthorized" element={<UnauthorizedPage />} />
       <Route path="/"             element={<HomeRedirect />} />
 
-      {/* Candidate */}
       <Route path="/jobs"            element={<PrivateRoute><JobsPage /></PrivateRoute>} />
-      <Route path="/jobs/:id"        element={<PrivateRoute><JobDetailPage /></PrivateRoute>} />
       <Route path="/my-applications" element={<CandidateRoute><MyApplicationsPage /></CandidateRoute>} />
 
-      {/* Recruiter */}
-      <Route path="/recruiter/dashboard"        element={<RecruiterRoute><RecruiterDashboardPage /></RecruiterRoute>} />
-      <Route path="/recruiter/jobs"             element={<RecruiterRoute><RecruiterJobsPage /></RecruiterRoute>} />
-      <Route path="/recruiter/jobs/create"      element={<RecruiterRoute><CreateJobPage /></RecruiterRoute>} />
-      <Route path="/recruiter/jobs/:id/edit"    element={<RecruiterRoute><EditJobPage /></RecruiterRoute>} />
-      <Route path="/recruiter/jobs/:id/applicants" element={<RecruiterRoute><JobApplicantsPage /></RecruiterRoute>} />
+      <Route path="/recruiter/dashboard" element={<RecruiterRoute><RecruiterDashboard /></RecruiterRoute>} />
+      <Route path="/recruiter/jobs"      element={<RecruiterRoute><RecruiterJobsPage /></RecruiterRoute>} />
 
       <Route path="*" element={<Navigate to="/" replace />} />
-
-      <Route path="/jobs/:jobId" element={<PrivateRoute><JobDetailPage /></PrivateRoute>} />
-      
     </Routes>
   )
 }
